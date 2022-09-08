14d799b1a84bb204672e5b7266cb0f66d61ea8f9
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-09-08 17:13:47 +0200

    Cleanup

--- src/menuActions/BalanceInquiry.java
+++ src/menuActions/BalanceInquiry.java
@@ -8,10 +8,12 @@ import java.util.List;
 public class BalanceInquiry {
     private final List<User> users;
     private final int index;
-    public BalanceInquiry(List<User> users, int index){
+
+    public BalanceInquiry(List<User> users, int index) {
         this.users = users;
         this.index = index;
     }
+
     public void viewBalance() {
         Display.clearScreen();
         System.out.println("Balance is: " + users.get(index).getBalance() + "SEK");
